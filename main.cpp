51dc32fc343f241ed83c1e26424c2896374ebfd0
Kasparas <159523672+Kaspa06@users.noreply.github.com>
2024-05-31 19:53:42 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,31 @@
+#include "functions.h"
 
+int main() {
+    std::ofstream OutputFile("output.txt");
+    std::ofstream OutputUrl("url.txt");
+
+    Words();
+
+    // Print repetitive words/count/reference to text.txt
+    OutputFile << "Words:\n";
+    for (const auto &entry : word_map) {
+        if(entry.second.first > 1) {
+            OutputFile << entry.first << ": " << entry.second.first << " [lines: ";
+            for (const int &ln : entry.second.second) {
+                OutputFile << ln << " ";
+            }
+            OutputFile << "]" << std::endl;
+        }
+    }
+
+    // Print the URLs and their count to url.txt
+    OutputUrl << "\nURLs:\n";
+    for (const auto &entry : url_map) {
+        OutputUrl << entry.first << std::endl;
+    }
+
+    OutputUrl.close();
+    OutputFile.close();
+
+    return 0;
+}
